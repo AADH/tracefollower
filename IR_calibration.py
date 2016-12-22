28318d8a5ab3f6465203e1b2e8be1d6e17048d32
Abran DeCarlo Hernandez <abranhernandez0@gmail.com>
2016-12-21 18:50:08 -0800

    Update IR_calibration.py

--- IR_calibration.py
+++ IR_calibration.py
@@ -1,5 +1,8 @@
 # Calibrates IR LEDS
 # Reports current resistance either every 5 seconds, press of a button, or a constant stream (hopefully this is the chosen one)
+import RPi.GPIO as GPIO
+
 x0 = 0  #x1-x3 are the IR LED sensors from left to right
 x1 = 0
 x2 = 0
+
